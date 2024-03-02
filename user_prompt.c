302e2e9d9e629afbf7b6e095893f8b628903a83a
Udoozy <agbo4samson@gmail.com>
2024-03-01 20:56:51 -0800

    Amended

--- user_prompt.c
+++ user_prompt.c
@@ -1,9 +1,34 @@
 #include "shell.h"
 /**
- * user_prompt - User defined pormpt function
+ * main - User defined pormpt function
+ * Return: Always 0
  */
 
-void user_prompt(void)
+int main(void)
 {
-	printf("s_j shell$ \n");
+	char *buffer = NULL;
+	size_t n = 0;
+	ssize_t line;
+
+	printf("sj_shell$ ");
+	while (1)
+	{
+		line = getline(&buffer, &n, stdin);
+		if (line == -1)
+		{
+			if (feof(stdin))
+			{
+				printf("End of File\n");
+			}
+			else
+			{
+				printf("Error Occured while reading input\n");
+			}
+			break;
+		}
+		printf("%s\n", buffer);
+		printf("sj_shell$ ");
+	}
+	free(buffer);
+	return (0);
 }
